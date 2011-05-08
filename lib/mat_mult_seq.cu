0701bfe65b23bcc0c5db2d5f00bf0a77de958fb3
shane <shane@shane-mahons-imac.local>
2011-05-08 13:36:20 -0500

    iterate the same way the GPU implementation does

--- lib/mat_mult_seq.cu
+++ lib/mat_mult_seq.cu
@@ -14,12 +14,12 @@ void MatMultSeq(Matrix *A, Matrix *B, Matrix *X, float alpha) {
 
   //initiallize known values of X[O]s based on B[0]s
   //we can do this because we know the diagonal (i==j) are all 1s in A
-  for (i = 0; i < n; i++)
-    X->els[i] = alpha*B->els[i];//  X[0][i] = alpha*B[0][i];
+  //for (i = 0; i < n; i++)
+  //  X->els[i] = alpha*B->els[i];//  X[0][i] = alpha*B[0][i];
 
   //calculate rest of X[1..n] values
-  for (i = 1; i < n; i++) {
-    for (j = 0; j < n; j++) {
+  for (j = 0; j < n; j++) {
+    for (i = 0; i < n; i++) {
       S = alpha*B->els[i*B->width+j]; // S = alpha*B[i][j];
       //printf("i=%d,j=%d, S=%f\n", i, j, S);
       for (k = 0; k < i; k++) {
