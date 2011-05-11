d50159d8222d092c41dedbb3c337fb7d47a77769
Aaron Stacy <aaron.r.stacy@gmail.com>
2011-05-11 16:16:36 -0500

    fixed another test_run.sh bug

--- bin/test_run.sh
+++ bin/test_run.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-rm -f obj/test_input_*.txt.*out obj/make_cuda.o*
+rm -f obj/test_input_*.txt.*out obj/make_cuda.o* obj/verify_output.txt
 
 qsub jobscripts/job_script.make_cuda
 
@@ -9,10 +9,9 @@ while [ "`qstat | wc -l 2>/dev/null`" -gt "0" ]; do
 	sleep 1
 done
 
-rm -f obj/verify_output.txt
 ( 
 	bin/verify_outputs.sh 
 	echo "done verifying outputs"
-)>obj/verify_output.txt &
+)&>obj/verify_output.txt &
 
 vim obj/make_cuda.o*
