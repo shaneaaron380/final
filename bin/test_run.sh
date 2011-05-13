708be6746d4f70f9c424b43ddf7c201c28017db9
Aaron Stacy <aaron.r.stacy@gmail.com>
2011-05-13 01:15:57 -0500

    more tweaks to test_run.sh

--- bin/test_run.sh
+++ bin/test_run.sh
@@ -1,17 +1,19 @@
 #! /bin/bash
 
-rm -f obj/test_input_*.txt.*out obj/make_cuda.o* obj/verify_output.txt
+rm -f obj/test_input_*.txt.*out obj/make_{cuda,all}.o* obj/verify_output.txt obj/verify_all.o*
 
-qsub jobscripts/job_script.make_cuda
+#qsub jobscripts/job_script.make_cuda
+qsub jobscripts/job_script.make_all
 
 while [ "`qstat | wc -l 2>/dev/null`" -gt "0" ]; do 
 	echo -n '.'
 	sleep 1
 done
 
-( 
-	bin/verify_outputs.sh 
-	echo "done verifying outputs"
-)&>obj/verify_output.txt &
+#( 
+#    bin/verify_outputs.sh 
+#    echo "done verifying outputs"
+#)&>obj/verify_output.txt &
+qsub jobscripts/job_script.verify_all
 
-vim obj/make_cuda.o*
+while ! tail -f obj/verify_all.o* 2>/dev/null; do sleep 0.1; done | grep -v ERROR
